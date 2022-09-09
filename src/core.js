c35b5d69f65cea21fcfe4811666a0d73bb479962
OlhaDorofiychuk <olha.d@yahoo.com>
2022-09-09 14:47:37 +0100

    updated

--- src/core.js
+++ src/core.js
@@ -21,10 +21,10 @@ person.instrument = ['Guitar', 'Piano', 'Vocals']
 const mainInstrument = person.instrument[2]
 
 // 7. Add a profession property to the person, which is an object
-person.profession = []
+person.profession = {}
 
 // 8. Add a name property to the profession object with the value Musician
-person.profession = ['Musician']
+person.profession.name = 'Musician'
 // 9. Add a friends property to the person, which is an empty arrays
 
 // 10. Add two objects to the persons friends array with the following properties:
