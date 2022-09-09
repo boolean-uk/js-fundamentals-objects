4f3841f4fb2c5ce55f7b3d4072e44978740869e8
Samuel Odigie <samodigie98@gmail.com>
2022-09-09 15:32:48 +0100

    first c

--- src/core.js
+++ src/core.js
@@ -2,47 +2,41 @@
 const person = { age: 32, size: 'Small' }
 
 // 1. Give the person object a name property with the value Matt
-
+person.name = 'Matt'
 
 // 2. Remove the size property from the person
-
+delete person.size
 
 // 3. Increase the person's age by 11
-
+person.age = 43
 
 // 4. Add an instruments property to the person, initialised as an empty array
-
+person.intruments = []
 
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-
+person.intruments.push = ('Guitar', 'Piano', 'Vocals')
 
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
 const mainInstrument = undefined
 
-
 // 7. Add a profession property to the person, which is an object
-
 
 // 8. Add a name property to the profession object with the value Musician
 
-
 // 9. Add a friends property to the person, which is an empty array
-
 
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
 
-
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
 const bestFriend = undefined
 
-
 // Don't change the code below this line
 module.exports = {
-    person,
-    mainInstrument,
-    bestFriend
+  person,
+  mainInstrument,
+  bestFriend
 }
