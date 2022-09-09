63597d11bfb93364c8e1e8f920131f5f8f947400
Dave <Daverevell491@gmail.com>
2022-09-09 15:04:45 +0100

    Question 9

--- src/core.js
+++ src/core.js
@@ -25,9 +25,10 @@ person.profession = {}
 
 // 8. Add a name property to the profession object with the value Musician
 person.profession = 'Musician'
-console.log(person)
 
 // 9. Add a friends property to the person, which is an empty array
+person.friends = []
+console.log(person)
 
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
