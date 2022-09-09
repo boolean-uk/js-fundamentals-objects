f457fed3b29d609b5494874d3764bfc561abe562
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-09-09 16:06:48 +0100

    third question

--- src/extension.js
+++ src/extension.js
@@ -41,7 +41,8 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
+console.log(numberOfVoucherCodes)
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
 const firstVoucherCode = null
