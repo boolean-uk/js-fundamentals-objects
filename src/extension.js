a098a5682aa38b6030368d995f5a4064e06dd0c5
MaybeFreak <michaelpope97@gmail.com>
2022-11-11 17:20:42 +0100

    Completed the work

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,14 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(`Should read Clean Code: ${name}`)
+console.log('--------')
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+console.log(`Should read 9780132350884: ${isbn10}`)
+console.log('--------')
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +46,64 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
+console.log(`Should read 2: ${numberOfVoucherCodes}`)
+console.log('--------')
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+console.log(`Should read AA-AA-A: ${firstVoucherCode}`)
+console.log('--------')
+
+// Work from the error codes
+
+// Changed to Category of the book to Programming instead of Cooking
+book.category = 'Programming'
+console.log(`Category should now be updated to Programming`)
+console.log(book)
+console.log('--------')
+
+// Changed the price of the Apples to 2 instead of 1
+basket.items[0].price = 2
+console.log(`Apples price should now be updated to 2`)
+console.log(basket)
+console.log('--------')
+
+// Deleted the dimensions form the book object
+delete book.dimensions
+console.log(`Dimensions should now be removed`)
+console.log(book)
+console.log('--------')
+
+// Created and set isbn13 in the isbn object of book
+book.isbn.isbn13 = '978-0132350884'
+console.log(`Book should now have added isbn13 with the value of 978-0132350884`)
+console.log(book.isbn)
+console.log('--------')
+
+// Created and set a pages value for book
+book.pages = 464
+console.log(`Book should now have a page value that is set to 464`)
+console.log(book)
+console.log('--------')
+
+// Added a new object for Oranges to the end of the basket.items array 
+basket.items.push({
+  name: 'Oranges',
+  quantity: 4,
+  price: 0.75
+})
+console.log(`The basket should have oranges added to its item array with name: oranges quantity: 4 and price: 0.75`)
+console.log(basket.items[2])
+console.log('--------')
+
+// Deleted the asin value form the book.isbn object
+delete book.isbn.asin
+console.log('Books isbn should no longer contain asin')
+console.log(book.isbn)
+console.log('--------')
+
+console.log()
 
 // Do not edit this exported object
 module.exports = {
@@ -56,3 +114,6 @@ module.exports = {
   numberOfVoucherCodes: numberOfVoucherCodes,
   firstVoucherCode: firstVoucherCode
 }
+
+const test = basket.items.findIndex(basket.items.name === 'Apple')
+console.log(test)
