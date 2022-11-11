8d2ae03f6700af4db357109311f681cb65c9dbf6
Kat <katdemey@outlook.com>
2022-11-11 15:50:01 +0000

    All extensions. We totally nailed it

--- src/extension.js
+++ src/extension.js
@@ -18,11 +18,16 @@ const book = {
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
 
+book.isbn.isbn13= isbn13
+console.log(book)
+
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+//console.log(name)
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+//console.log(isbn10)
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +47,31 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
+//console.log(numberOfVoucherCodes)
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+//console.log(firstVoucherCode)
+
+// 5. other failure corrections
+//Basket
+basket.items.push({name: 'Oranges', price: 0.75, quantity: 4})
+//console.log(basket.items)
+
+basket.items[0].price = 2
+
+//Book 
+delete book.isbn.asin
+//console.log(book)
+
+book.category = 'Programming'
+//console.log(book.category)
+
+delete book.dimensions
+
+book.pages = 464
+
 
 // Do not edit this exported object
 module.exports = {
