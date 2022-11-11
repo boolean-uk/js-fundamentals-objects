9549f30010cfff8101fb59614b7f3400b6096c0b
Chris Sach <christophersach90@gmail.com>
2022-11-11 15:02:51 +0000

    core 1-5

--- src/core.js
+++ src/core.js
@@ -2,15 +2,18 @@
 const person = { age: 32, size: 'Small' }
 
 // 1. Give the person object a name property with the value Matt
-
+person.name = 'Matt'
 // 2. Remove the size property from the person
-
+delete person.size
 // 3. Increase the person's age by 11
+person.age++
+console.log(person)
 
 // 4. Add an instruments property to the person, initialised as an empty array
-
+person.instruments = []
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-
+person.instruments.push('Guitar', 'Piano', 'Vocals')
+console.log(person)
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
 const mainInstrument = undefined
