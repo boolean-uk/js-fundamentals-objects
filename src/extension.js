9ba75d5be3db5fa9b25d7611cb533280b40dece0
davidbrockway <dav.brock@protonmail.com>
2023-03-03 13:16:54 +0000

    Completed Extension - Test Passed!

--- src/extension.js
+++ src/extension.js
@@ -18,11 +18,31 @@ const book = {
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
 
+// book isbn13 add object inside the book object
+book.isbn.isbn13 = isbn13;
+
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(book)
+
+delete book.isbn.asin
+console.log(book)
+
+// Set Book pages - to 464
+book.pages = 464;
+console.log(book.pages)
+
+//
+
+book.category = 'Programming';
+console.log(book.category);
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+// remove dimensions key
+delete book.dimensions
+console.log(book)
 
 // Do not modify this basket object directly
 const basket = {
@@ -41,11 +61,24 @@ const basket = {
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
+const apple = basket.items.find(item => item.name === 'Apple');
+if (apple) {
+  apple.price = 2;
+}
+
+// Adds orrranges to exisiting item basket list
+
+basket.items.push({
+  name: 'Oranges',
+  quantity: 4,
+  price: 0.75
+});
+
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length;
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0];
 
 // Do not edit this exported object
 module.exports = {
