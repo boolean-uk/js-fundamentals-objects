0fea7f7c2fcdac29f5f8869c650ced17358cc96f
Priya <ps975076@gmail.com>
2023-03-03 16:21:31 +0000

    updated objects

--- src/core.js
+++ src/core.js
@@ -18,21 +18,21 @@ person.instruments = [];
 
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
 person.instruments.push('Guitar', 'Piano', 'Vocals');
-console.log(person.instrument)
-
+console.log(person.instruments)
+// console.log(person)
 
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
-const mainInstrument = person.instrument[2];
-
+const mainInstrument = person.instruments[2];
+console.log(mainInstrument)
 
 // 7. Add a profession property to the person, which is an object
 person.profession = {};
 
-
 // 8. Add a name property to the profession object with the value Musician
 person.profession.name = 'Musician';
-console.log(person.profession.names)
+console.log(person.profession.name);
+// console.log(person)
 
 // 9. Add a friends property to the person, which is an empty array
 person.friends = [];
