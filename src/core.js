979b32722c39dab8443215a1d2bbb23fcf742321
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-05 18:41:20 +0200

    changes some formatting and done extension

--- src/core.js
+++ src/core.js
@@ -2,9 +2,7 @@
 const person = { age: 32, size: 'Small' }
 
 // 1. Give the person object a name property with the value Matt
-person.name = "Matt"
-
-
+person.name = 'Matt'
 
 // 2. Remove the size property from the person
 delete person.size
@@ -15,13 +13,8 @@ person.age = 43
 // 4. Add an instruments property to the person, initialised as an empty array
 person.instruments = []
 
-
-
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-person.instruments = ["Guitar", "Piano", "Vocals"]
-
-
-
+person.instruments = ['Guitar', 'Piano', 'Vocals']
 
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
@@ -31,9 +24,7 @@ const mainInstrument = person.instruments[2]
 person.profession = {}
 
 // 8. Add a name property to the profession object with the value Musician
-person.profession.name = "Musician"
-
-
+person.profession.name = 'Musician'
 
 // 9. Add a friends property to the person, which is an empty array
 person.friends = []
@@ -41,18 +32,12 @@ person.friends = []
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
-person.friends.push({ name: "Chris", age: 46})
-person.friends.push({name: "Dom", age: 43})
-
-
-
-
+person.friends.push({ name: 'Chris', age: 46 })
+person.friends.push({ name: 'Dom', age: 43 })
 
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
 const bestFriend = person.friends[0].name
-
-
 
 // Don't change the code below this line
 module.exports = {
