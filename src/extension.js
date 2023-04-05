b400d6af88a4b8e49d79c7484833094a7ddcbd29
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-05 17:13:14 +0300

    Nikolaos Giagkinis second half of extension

--- src/extension.js
+++ src/extension.js
@@ -42,10 +42,10 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
 // Do not edit this exported object
 module.exports = {
