ab1225766864799cdbfce5eb6235efb442b54e9d
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-05 20:58:44 +0200

    Best team fundamentals

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,12 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(name)
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+console.log(isbn10)
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +44,19 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
+console.log(numberOfVoucherCodes)
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+console.log(firstVoucherCode)
+
+// 5. apple price to 2
+basket.items[0].price = 2
+console.log(basket)
+
+// 6. oranges end of item list
+
 
 // Do not edit this exported object
 module.exports = {
