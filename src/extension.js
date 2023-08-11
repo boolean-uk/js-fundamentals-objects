aa659870181f848d0a7afad6ba5792a5040fc74d
Ali Keskin <cmdalikeskin@gmail.com>
2023-08-12 00:06:40 +0200

    Ali Keskin added extensions

--- src/extension.js
+++ src/extension.js
@@ -47,6 +47,94 @@ console.log(numberOfVoucherCodes)
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
 const firstVoucherCode = basket.voucherCodes[0]
 console.log(firstVoucherCode)
+
+
+//Read from the website
+
+// 1) Extension: Book category should be Programming
+//   Message:
+//     Expected 'Cooking' to equal 'Programming'.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:33:35)
+//         at <Jasmine>
+
+book.category = "Programming"
+
+
+
+
+// 2) Extension: Book should not contain the dimensions key - it should be deleted
+//   Message:
+//     Expected '10x12x2' not to be defined.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:45:41)
+//         at <Jasmine>
+delete book.dimensions
+
+
+
+
+// 3) Extension: Book should not contain the asin key - it should be deleted
+//   Message:
+//     Expected '0132350882' not to be defined.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:49:40)
+//         at <Jasmine>
+
+delete book.isbn.asin
+
+
+
+// 4) Extension: Book ISBN 13 should be 978-0132350884
+//   Message:
+//     Expected undefined to equal '978-0132350884'.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:41:38)
+//         at <Jasmine>
+
+book.isbn.isbn10 = "978-0132350884"
+
+
+// 5) Extension: Book pages should be 464
+//   Message:
+//     Expected undefined to equal 464.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:37:32)
+//         at <Jasmine>
+
+book.pages = 464
+
+
+
+// 6) Extension: 4 oranges priced at 0.75 should be added to the end of the items list
+//   Message:
+//     Expected undefined to equal Object({ name: 'Oranges', price: 0.75, quantity: 4 }).
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:17:37)
+//         at <Jasmine>
+
+basket.items.push({ name: 'Oranges', price: 0.75, quantity: 4  })
+
+
+
+// 7) Extension: The price of apples should be updated to 2
+//   Message:
+//     Expected 1 to equal 2.
+//   Stack:
+//         at <Jasmine>
+//         at UserContext.<anonymous> (/home/runner/work/js-fundamentals-objects/js-fundamentals-objects/spec/extension.spec.js:13:43)
+//         at <Jasmine>
+basket.items[0].price = 2
+
+
+console.table(basket.items)
+
 // Do not edit this exported object
 module.exports = {
   name,
