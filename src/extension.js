70e971357dc48725b80072d15ae983dfd9b37dc7
Ävadakedavr0 <dylandd4@hotmail.be>
2023-08-14 12:41:52 +0200

    extensions

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,16 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+book.category = 'Programming'
+delete book.dimensions
+book.isbn.isbn13 = isbn13
+book.pages = 464
+delete book.isbn.asin
 
 // Do not modify this basket object directly
 const basket = {
@@ -40,12 +46,20 @@ const basket = {
   ],
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
+const basketChanges = basket
+basketChanges.items.push({
+  name: 'Oranges',
+  quantity: 4,
+  price: 0.75
+})
+const appleDouble = basketChanges.items.find((i) => i.name === 'Apple')
+if (appleDouble) appleDouble.price = 2
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
 // Do not edit this exported object
 module.exports = {
