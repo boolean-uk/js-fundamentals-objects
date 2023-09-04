0e1fa2f7ebe51ccb82bf2470f82a95bb3b463bc6
SukunimiVinod <Vinodvinod19032022@gmail.com>
2023-09-04 19:32:56 +0300

    done extention

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,12 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(name)
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+console.log(isbn10)
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +44,12 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes =basket.voucherCodes.length
+console.log(numberOfVoucherCodes)
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+console.log(firstVoucherCode)
 
 // Do not edit this exported object
 module.exports = {
@@ -56,3 +60,24 @@ module.exports = {
   numberOfVoucherCodes,
   firstVoucherCode
 }
+
+delete book.isbn.asin
+
+book .isbn.isbn13 = isbn13
+console.log(book)
+
+book.pages = 464
+console.log(book)
+
+delete book.dimensions
+console.log(book)
+
+basket .items.push( {
+  name : 'Oranges',
+  quantity : 4,
+  price : 0.75
+}
+)
+basket .items[0].price = 2
+console.log()
+book .category = 'Programming'
