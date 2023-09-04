55f77460741efd244756730037e480ea559b46bd
Callum Hayden <callumhayden@callums-mbp.home>
2023-09-04 14:54:06 +0100

    4 failures left

--- src/extension.js
+++ src/extension.js
@@ -17,9 +17,11 @@ const book = {
 
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
+book.isbn.isbn13 = isbn13
 
 // 1. Set this to the book name - using the book object
 const name = book.name
+delete book.isbn.asin
 
 // 2. Set this to the isbn 10 value - using the book object
 const isbn10 = book.isbn.isbn10
@@ -43,6 +45,7 @@ const basket = {
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
 const numberOfVoucherCodes = basket.voucherCodes.length
+basket.item[0].price = 2
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
 const firstVoucherCode = basket.voucherCodes[0]
