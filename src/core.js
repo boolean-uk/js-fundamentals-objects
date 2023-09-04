a89501287897354414ce0e0e16a25da6a2b1b37d
Rebecca Noy <rebecca.noy@gmx.de>
2023-09-04 16:33:39 +0200

    Pushing again, removed semi-colons

--- src/core.js
+++ src/core.js
@@ -10,15 +10,15 @@ person.age += 11
 // 4. Add an instruments property to the person, initialised as an empty array
 person.instruments = []
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-person.instruments.push(`Guitar`, `Piano`, `Vocals`);
+person.instruments.push(`Guitar`, `Piano`, `Vocals`)
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
-const mainInstrument = person.instruments[2];
+const mainInstrument = person.instruments[2]
 // 7. Add a profession property to the person, which is an object
 person.profession = {}
 
 // 8. Add a name property to the profession object with the value Musician
-person.profession.name = `Musician`;
+person.profession.name = `Musician`
 
 // 9. Add a friends property to the person, which is an empty array
 person.friends = []
