3e867e6ecdfcd55cd9d450a93aee74b218e20ab3
André Sturesson <andre.m.sturesson@gmail.com>
2024-02-15 11:58:23 +0100

    Core + extensions

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,16 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+book.pages = 464
+book.isbn.asin = ''
+book.category = 'Programming'
+book.isbn.isbn13 = isbn13
+delete book.dimensions
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+delete book.isbn.asin
 
 // Do not modify this basket object directly
 const basket = {
@@ -40,12 +46,20 @@ const basket = {
   ],
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
+const oranges = {
+  name: 'Oranges',
+  quantity: 4,
+  price: 0.75
+}
+basket.items[0].price = 2
+
+basket.items.push(oranges)
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
 // Do not edit this exported object
 module.exports = {
