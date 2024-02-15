4cd671d35bc03ce97d8aa8851241fcf24a21f054
Victor <victor.adamson@se.experis.com>
2024-02-15 16:10:30 +0100

    Core + Extensions

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,17 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+delete book.dimensions
+book.pages = 464
+delete book.isbn.asin
+book.category = 'Programming'
+book.isbn.isbn13 = isbn13
+// console.log(book)
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,11 +49,15 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
+basket.items[0].price = 2
+const oranges = { name: 'Oranges', quantity: 4, price: 0.75 }
+basket.items.push(oranges)
+console.log(basket)
 // Do not edit this exported object
 module.exports = {
   name,
