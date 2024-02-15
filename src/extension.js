aa7255d240841b4a0db2162f3cc91bb43341aff3
Marcus Palm <marcus.palm@se.experis.com>
2024-02-15 16:34:12 +0100

    Marcus Palm done with objects

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,19 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+// book changes from test fail messages
+book.category = 'Programming'
+delete book.dimensions
+// eslint-disable-next-line dot-notation
+book.isbn['isbn13'] = isbn13
+delete book.isbn.asin
+
+book.pages = 464
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +51,14 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+
+// adding changes to basket from test fail messages
+basket.items.push({ name: 'Oranges', price: 0.75, quantity: 4 })
+basket.items[0].price = 2
 
 // Do not edit this exported object
 module.exports = {
