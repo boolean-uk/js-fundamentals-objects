53c274e0a3b139f77e7161c8cd40b459d9c1fbfc
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-02-16 14:55:32 +0100

    chaniging required have been done and passing the test

--- src/extension.js
+++ src/extension.js
@@ -15,6 +15,11 @@ const book = {
   dimensions: '10x12x2'
 }
 
+book.category = 'Programming'
+delete book.isbn.asin
+book.isbn.isbn13 = '978-0132350884'
+delete book.dimensions
+book.pages = 464
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
 
@@ -41,12 +46,15 @@ const basket = {
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
+basket.items.push({ name: 'Oranges', price: 0.75, quantity: 4 })
+basket.items[0].price = 2
+
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
 const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
 const firstVoucherCode = basket.voucherCodes[0]
-
+console.log(firstVoucherCode)
 // Do not edit this exported object
 module.exports = {
   name,
