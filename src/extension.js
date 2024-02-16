af313c0a61a81f3f9d2006ef7483716e10e1a968
HomoNOVISCoding <mama.leye@gmail.com>
2024-02-16 21:21:46 +0000

    extension a bit tough NGL

--- src/extension.js
+++ src/extension.js
@@ -23,7 +23,7 @@ const name = book.name
 console.log(name)
 // 2. Set this to the isbn 10 value - using the book object
 const isbn10 = book.isbn.isbn10
-
+console.log(isbn10.indexOf(0))
 // Do not modify this basket object directly
 const basket = {
   items: [
@@ -43,10 +43,10 @@ const basket = {
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
 const numberOfVoucherCodes = basket.voucherCodes.length
-
-// 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = basket.voucherCodes
-console.log(firstVoucherCode)
+console.log(numberOfVoucherCodes)
+// 4. Set this variable to the first element in of the baskets voucher Codes array - using the basket object
+const firstVoucherCode = basket.voucherCodes[0]
+console.log("'" + firstVoucherCode + "'")
 // Do not edit this exported object
 module.exports = {
   name,
