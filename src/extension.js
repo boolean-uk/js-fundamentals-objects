76acda84d59a1ebab2d655d7e87525a565e1ab8c
Kanthee Kosolyuthasarn <Kanthee>
2024-02-16 14:47:27 +0100

    Kanthee Kosolyuthasarn - C#3 - 16.02.24: Core + extension ++

--- src/extension.js
+++ src/extension.js
@@ -18,11 +18,17 @@ const book = {
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
 
+book.category = 'Programming'
+book.pages = 464
+delete book.isbn.asin
+book.isbn.isbn13 = isbn13
+delete book.dimensions
+
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
 
 // Do not modify this basket object directly
 const basket = {
@@ -30,7 +36,7 @@ const basket = {
     {
       name: 'Apple',
       quantity: 10,
-      price: 1
+      price: 2
     },
     {
       name: 'Lemon',
@@ -41,11 +47,13 @@ const basket = {
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
+basket.items.push({ name: 'Oranges', price: 0.75, quantity: 4 })
+
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
 // Do not edit this exported object
 module.exports = {
