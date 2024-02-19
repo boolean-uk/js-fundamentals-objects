4e058f91f8bf782c100af0f231cb37226eb13eeb
Santhia <santhia.shanmukarasan@gmail.com>
2024-02-19 09:06:00 +0100

    All tasks completed

--- src/extension.js
+++ src/extension.js
@@ -18,11 +18,14 @@ const book = {
 /* eslint-disable no-unused-vars */
 const isbn13 = '978-0132350884'
 
+// Ensure the book ISBN 13 is '978-0132350884'
+book.isbn.isbn13 = '978-0132350884'
+
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name; 
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10;
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +45,39 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length;
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0];
+
+
+/*update price of apples to be 2*/
+// Find the index of the item with the name 'Apple'
+const index = basket.items.findIndex(item => item.name === 'Apple');
+
+// Update the price of apples if found
+if (index !== -1) {
+    basket.items[index].price = 2;
+}
+
+//Add 4 oranges priced at 0.75 to the end of the items list in the basket
+basket.items.push({
+  name : 'Oranges',
+  quantity: 4,
+  price: 0.75
+});
+
+//Ensure the book category is 'Programming'
+book.category = 'Programming';
+
+//Remove the dimentions key from the book object
+delete book.dimensions;
+
+//Remove the asin key from the isbn ovject in the book
+delete book.isbn.asin;
+
+// Ensure the book pages are 464
+book.pages = 464;
 
 // Do not edit this exported object
 module.exports = {
