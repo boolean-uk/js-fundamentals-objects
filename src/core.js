001ec1f0c2b6364c504224dc712bb14127892cfe
Ateeb <ateeb@live.no>
2024-02-24 17:24:18 +0100

    fixed

--- src/core.js
+++ src/core.js
@@ -14,21 +14,19 @@ person.age += 11;
 
 // 4. Add an instruments property to the person, initialised as an empty array
 
-person.instrument = {};
+person.instruments = [];
 
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
-person.instrument[0]= 'Guitar';
-person.instrument[1]= 'Piano';
-person.instrument[2]= 'Vocals';
+person.instruments.push('Guitar', 'Piano', 'Vocals');
 
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
 
-const mainInstrument = person.instrument[2];
+const mainInstrument = person.instruments[2];
 
 // 7. Add a profession property to the person, which is an object
 
-person.profession = [];
+person.profession = {};
 
 // 8. Add a name property to the profession object with the value Musician
 
@@ -36,16 +34,14 @@ person.profession.name = 'Musician';
 
 // 9. Add a friends property to the person, which is an empty array
 
-person.friends = {};
+person.friends = [];
 
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
 
-person.friends[0].name = 'Chris';
-person.friends[0].age = 46;
-person.friends[1].name = 'Dom';
-person.friends[1].age = 43;
+person.friends.push({ name: 'Chris', age: 46 }, { name: 'Dom', age: 43 });
+
 
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
