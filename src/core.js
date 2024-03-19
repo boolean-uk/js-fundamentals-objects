ce31a5e1248fc63e1afb80fe6f53ec273dfdb5f1
Tim Zoltie <t.zoltie@gmail.com>
2024-03-19 18:51:30 +0000

    advanced

--- src/core.js
+++ src/core.js
@@ -19,27 +19,27 @@ person.instruments.push('Guitar', 'Piano', 'Vocals')
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
 const mainInstrument = person.instruments[2]
-console.log(mainInstrument)
+
 // 7. Add a profession property to the person, which is an object
 person.profession = {}
-console.log(person)
+
 // 8. Add a name property to the profession object with the value Musician
 person.profession = { name: 'Musician' }
-console.log(person)
+
 // 9. Add a friends property to the person, which is an empty array
 person.friends = []
-console.log(person)
+
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
 const friendOne = { name: 'Chris', age: 46 }
 const friendTwo = { name: 'Dom', age: 43 }
 person.friends.push(friendOne, friendTwo)
-console.log(person)
+
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
 const bestFriend = person.friends[0].name
-console.log(bestFriend)
+
 // Don't change the code below this line
 module.exports = {
   person,
